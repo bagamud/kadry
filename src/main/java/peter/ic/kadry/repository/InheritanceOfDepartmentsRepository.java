28a1c9a010dca3d80af17e539ba547d68550993b
Ponkratov K <ponkratovk@gmail.com>
2022-02-15 15:46:00 +0300

    Entity, Controllers, Views

---
+++ src/main/java/peter/ic/kadry/repository/InheritanceOfDepartmentsRepository.java
@@ -0,0 +1,12 @@
+package peter.ic.kadry.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import peter.ic.kadry.entity.InheritanceOfDepartments;
+
+import java.util.ArrayList;
+
+public interface InheritanceOfDepartmentsRepository extends CrudRepository<InheritanceOfDepartments, Integer> {
+    @Query("SELECT parent.code FROM InheritanceOfDepartments WHERE department.code = :code")
+    ArrayList<Integer> findInheritance(int code);
+}
